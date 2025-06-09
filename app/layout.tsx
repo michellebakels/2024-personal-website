fbe4717fa34cd73796f79d4445a9d31c5789d24d
michellebakels <michelle.bakels@gmail.com>
2025-06-09 13:44:21 -0400

    add semicolons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,29 @@
-import type { Metadata } from "next"
-import localFont from "next/font/local"
-import { Analytics } from '@vercel/analytics/react'
-import "./globals.css"
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/react";
+import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
-})
+});
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-})
+});
 
 export const metadata: Metadata = {
   title: "Michelle Bakels",
   description:
     "Software Developer, Community Builder, and Conference Organizer",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -34,5 +34,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  )
+  );
 }
